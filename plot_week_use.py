6f55ab11880bac2e1c66255e3b8fa64b1d470131
Hermes <hermes.espinola@wizeline.com>
2018-03-17 18:15:40 -0600

    Plot week use

--- plot_week_use.py
+++ plot_week_use.py
@@ -11,8 +11,7 @@ big_data['Fin_del_viaje'] = big_data['Fin_del_viaje'].map(lambda date_str: datet
 big_data['dia_semana'] = big_data['Inicio_del_viaje'].map(lambda date: date.weekday())
 big_data['hour'] = big_data.Inicio_del_viaje.map(lambda date: date.hour)
 
-for x in range(7):
-    n, bins, _ = plt.hist(big_data.loc[(big_data.dia_semana == x), 'hour'], bins=24, histtype='step')
-plt.show()
+hist = big_data.groupby(['hour', 'dia_semana']).agg(['mean', 'count'])['Viaje_Id']['count']
+hist.unstack().plot()
 
 print (big_data)
