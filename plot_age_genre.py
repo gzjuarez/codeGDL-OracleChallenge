50f333885d69430d45b151b62b1dc3169bdb3c53
Hermes <hermes.espinola@wizeline.com>
2018-03-18 05:50:30 -0600

    Plot kmeans clusters and data, Barro!: Corre Kmeans dos veces independiente entre hombre y mujer

--- plot_age_genre.py
+++ plot_age_genre.py
@@ -1,7 +1,9 @@
 import pandas as pd
-import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+from sklearn.cluster import KMeans
+import math
 
 import read_data
 
@@ -10,15 +12,55 @@ def calculate_intervals(max, min, size):
     k = 1 + np.log2(size)
     return rang / k
 
-data = read_data.read_all_data()
+data = read_data.read_sample_data()
 
 #print(pd.cut(birth_year, intervals))
 #data_filtered_birth = data[ len(data['Ano_de_nacimiento'].str) == 4]
+data['Ano_de_nacimiento'] = data['Ano_de_nacimiento'].astype(str)
+data_filtered_birth = data.filter(regex='^\d{4}$', axis=0)
 
-data_filtered_birth = data.set_index('Ano_de_nacimiento').filter(regex='^\d{4}$', axis=0)
-grouped = data_filtered_birth.groupby(['Ano_de_nacimiento', 'Genero']).agg('count')['Viaje_Id']
+list_genre = data_filtered_birth['Genero'].values.tolist()
+list_estacion_origen = data_filtered_birth['Origen_Id'].values.tolist()
+list_nacimiento = data['Ano_de_nacimiento'].filter(regex='^\d{4}$').astype('float64').values.tolist()
+list_nacimiento = list(map(lambda n: math.floor((2017 - n) / 10) * 10, list_nacimiento))
 
-print(grouped)
+list_k = []
+
+for i in range(len(list_nacimiento)):
+    tup = (list_genre[i], list_estacion_origen[i], list_nacimiento[i])
+    list_k.append(tup)
+
+colors =  np.array(['k', 'r', 'b', 'g', 'y', 'c', 'm', 'lime', 'hotpink', 'teal'])
+markers = np.array(['x', 'o', '^', '*', 'h', 's', 'D', 'P', '8', '4'])
+X = np.array(list_k)
+kmeans = KMeans(n_clusters=10, random_state=1).fit(X)
+
+plt.plot(list_estacion_origen[list_genre == -1], list_nacimiento[list_genre == -1])
+plt.plot(list_estacion_origen[list_genre == 1], list_nacimiento[list_genre == 1])
+plt.show()
+
+input()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+for i, coords in enumerate(kmeans.cluster_centers_):
+    ax.scatter(coords[0], coords[1], coords[2], c=colors[i%10], marker='X')
+
+for x, y, z, label in zip(list_genre, list_estacion_origen, list_nacimiento, kmeans.labels_):
+    ax.scatter(x, y, z, c=colors[label], marker=markers[label])
+
+ax.set_xlabel('Genero')
+ax.set_ylabel('Estacion')
+ax.set_zlabel('Nacimiento')
+
+plt.show()
+
+#data_filtered_birth['Ano_de_nacimiento'] = data_filtered_birth['Ano_de_nacimiento'].astype("float64")
+# grouped = data_filtered_birth.groupby(['Origen_Id', 'Ano_de_nacimiento', 'Genero']).agg('count')['Viaje_Id']
+
+#print(grouped)
+#input()
+#print(grouped.unstack())
 
 #grouped = data.groupby(pd.cut(data['Ano_de_nacimiento'], 10))
 #print(pd.cut(birth_year.astype('int64'), 50))
--- read_data.py
+++ read_data.py
@@ -4,18 +4,18 @@ import numpy as np
 
 GENRE_POS = 2
 
-def read_all_data():    
+def read_all_data():
     data_location = r'Data'
     all_data = glob.glob( os.path.join(data_location, "datos_abiertos_*.csv") )
 
     df_from_each_file = (pd.read_csv(f, low_memory = False) for f in all_data)
     concatenated_df = pd.concat(df_from_each_file)
-    concatenated_df.Genero = concatenated_df.Genero.map(lambda g: 1 if g == 'H' else -1)
+    concatenated_df.Genero = concatenated_df.Genero.map(lambda g: 1 if g == 'H' else 0)
 
     return concatenated_df.dropna(how='any')
 
-def read_sample_data():    
-    
+def read_sample_data():
+
     df = pd.read_csv("Data/datos_abiertos_2017_01.csv",  low_memory = False)
     df.Genero = df.Genero.map(lambda g: 1 if g == 'H' else -1)
     #print(df.Genero.sum())
