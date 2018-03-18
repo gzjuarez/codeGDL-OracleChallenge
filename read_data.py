9cd082d0f5356cc710d9ef868386dd9e689d979b
geraJuarez <geratrex781@gmail.com>
2018-03-17 19:01:34 -0600

    change data to read

--- read_data.py
+++ read_data.py
@@ -1,13 +1,16 @@
 import glob, os
 import pandas as pd
+import numpy as np
 
 def read_data():    
     data_location = r'Data'
-    all_data = glob.glob( os.path.join(data_location, "*.csv") )
+    all_data = glob.glob( os.path.join(data_location, "datos_abiertos_*.csv") )
 
     df_from_each_file = (pd.read_csv(f, low_memory = False) for f in all_data)
-    concatenated_df = pd.concat(df_from_each_file, ignore_index=True)
+    concatenated_df = pd.concat(df_from_each_file)
 
-    return concatenated_df
+    #return concatenated_df
     #df = pd.read_csv(data_location, low_memory = False)
-    #print(concatenated_df)
+    print(concatenated_df.size)
+
+read_data()
